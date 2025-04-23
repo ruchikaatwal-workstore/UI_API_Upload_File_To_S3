51b1e210ed3fd892b9d0961333c17d8196e51d41
ruchikaatwal-workstore <Ruchika.Atwal@workstore.in>
2025-04-23 05:06:15 +0000

    modified port to 5002

--- app.py
+++ app.py
@@ -167,5 +167,5 @@ def delete_files():
 
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export FLASK_APP=app.py
 export FLASK_ENV=development
-flask run --host=0.0.0.0 --port=5001
+flask run --host=0.0.0.0 --port=5002
